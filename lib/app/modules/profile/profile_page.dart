aa91a127fb8fb6bf14df1ceb122106bd29dbf89e
Gustavo <gustavo10ssouza@gmail.com>
2020-07-30 21:36:24 -0300

    add configuration with firebase

--- lib/app/modules/profile/profile_page.dart
+++ lib/app/modules/profile/profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:app_sintec/app/modules/profile/widgets/customBarProfile.dart';
-import 'package:app_sintec/app/modules/profile/widgets/listViewConsultation.dart';
+import 'package:app_sintec/app/modules/profile/widgets/listViewCustomProfile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'profile_controller.dart';
